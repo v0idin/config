614ff540833226b0241a385f42d9eaeacd5f6b83
v0idin <voidinexile@gmail.com>
2022-11-08 03:02:30 +0200

    added hst config with kickstart and ssh configs

---
+++ hst/kickstart.sh
@@ -0,0 +1,13 @@
+apt update && apt dist-upgrade -y
+apt install -y plocate mc nftables
+updatedb
+
+mv /etc/ssh/sshd_config /etc/ssh/sshd_config_init
+cp sshd_config /etc/ssh/sshd_config
+
+ufw allow 52084/tcp
+ufw default deny incoming
+ufw default allow outgoing
+ufw enable
+
+systemctl restart sshd
